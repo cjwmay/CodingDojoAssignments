b76311e89182ae15818a5e06dda103624575bc3d
Your Name <cjwmay@live.cn>
2017-03-30 01:34:59 -0700

    update on3.30

---
+++ JS Basic/I_Do_Declare.js
@@ -0,0 +1,27 @@
+var a=0;
+var b=10;
+var c=1;
+var d=0.1;
+
+console.log("1stnum=", a);
+console.log("2ndnum=", b);
+console.log("3rdnum=", c);
+console.log("4thnum=", d);
+
+var a="hey";
+var b="oh";
+var c="aha";
+
+console.log("1ststring=", a);
+console.log("2ndstring=", b);
+console.log("3rdstring=", c);
+
+
+var a=true;
+var b=false;
+
+console.log("1stboolean", a);
+console.log("2ndboolean", b);
+
+var a=undefined;
+console.log("undefined=", a);
